4657e1558e5a741c594e78cb10f4caf402466c71
lixin <2237498407@qq.com>
2023-04-12 22:25:33 +0800

    初始化提交

---
+++ src/main/java/indl/lixn/tickingelapse/TickingElapseApplication.java
@@ -0,0 +1,13 @@
+package indl.lixn.tickingelapse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickingElapseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickingElapseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indl/lixn/tickingelapse/TickingElapseApplicationTests.java
@@ -0,0 +1,13 @@
+package indl.lixn.tickingelapse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickingElapseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
