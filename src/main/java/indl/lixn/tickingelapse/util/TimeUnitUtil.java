74a02884465da56bb3f54ab728055270b74f30c4
lixin <2237498407@qq.com>
2023-04-19 19:58:03 +0800

    修改为层级时间轮

--- src/main/java/indl/lixn/tickingelapse/util/TimeUnitUtil.java
+++
@@ -1,9 +0,0 @@
-package indl.lixn.tickingelapse.util;
-
-import indl.lixn.tickingelapse.common.TimeUnitSupport;
-
-/**
- * @author listen
- **/
-public class TimeUnitUtil extends TimeUnitSupport {
-}
